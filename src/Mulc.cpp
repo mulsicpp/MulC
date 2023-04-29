1a84782e9753cf66b58c5451277f5f4aec3ceaa5
Christian Mulser <christianmulser000@gmail.com>
2023-04-30 00:12:57 +0200

    initializing builder

--- src/Mulc.cpp
+++ src/Mulc.cpp
@@ -1,3 +1,12 @@
+#if defined(_WIN32)
+#include "windows.h"
+#endif
+
+#if defined(__linux__)
+#include <unistd.h>
+#include <linux/limits.h>
+#endif
+
 #include "Mulc.h"
 
 #include <stdio.h>
@@ -54,14 +63,54 @@ A:
 
     mode.arch = flags.arch;
     mode.config = flags.config;
-
-    printf("%i %i\n", flags.force, flags.run);
 }
 
 void Mulc::init(void)
 {
+    initialPath = std::filesystem::canonical(std::filesystem::current_path());
+
+#if defined(_WIN32)
+    char result[MAX_PATH];
+    GetModuleFileNameA(NULL, result, MAX_PATH);
+#endif
+
+#if defined(__linux__)
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+#endif
+    builderPath = std::filesystem::canonical(std::filesystem::path(result).parent_path());
+
+    if (std::filesystem::is_directory(flags.path))
+    {
+        std::vector<std::filesystem::path> scriptFiles;
+
+        auto iterator = std::filesystem::directory_iterator(flags.path);
+        for (const auto &entry : iterator)
+        {
+            if (entry.is_regular_file() && entry.path().extension() == ".mulc")
+                scriptFiles.push_back(entry.path());
+        }
+        if (scriptFiles.size() == 1)
+            buildFilePath = scriptFiles[0].string();
+        else if (scriptFiles.size() == 0)
+            error("No build script found");
+        else
+        {
+            error("Build script is ambiguous");
+        }
+    }
+    else
+    {
+        if (std::filesystem::path(flags.path).extension() == ".mulc")
+            buildFilePath = flags.path;
+        else
+            error("The specified file \'%s\' is not a build script", flags.path.c_str());
+    }
+
+    buildFilePath = std::filesystem::canonical(buildFilePath);
 }
 
-void Mulc::runScript(void) {
+void Mulc::runScript(void)
+{
     ScriptAPI::runScript();
 }
